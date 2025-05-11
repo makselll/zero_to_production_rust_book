409d070192744543202744fe4fbf9015613ec8e2
Max.Shel <maxs200099@gmail.com>
2025-05-11 19:25:01 +0700

    jaeger integration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,11 +2,6 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
@@ -23,22 +18,17 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
-# Launch postgres using Docker
+
+# Start the services using Docker Compose
 if [[ -z "${SKIP_DB}" ]]
 then
-  docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d postgres \
-      postgres -N 1000
+  docker compose up -d
 fi
 
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
   >&2 echo "Postgres is still unavailable - sleeping"
-sleep 1
+  sleep 1
 done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
@@ -46,6 +36,5 @@ done
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
->&2 echo "Postgres has been migrated, ready to go!"
 
-docker run -d -e COLLECTOR_OTLP_ENABLED=true -p 16686:16686 -p 4317:4317 jaegertracing/all-in-one:1.38.0
+>&2 echo "Postgres has been migrated, ready to go!"
